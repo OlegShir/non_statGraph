1828513c2d662a7e976e459ebf40d8d0e61d263d
OlegShir <63945818+OlegShir@users.noreply.github.com>
2021-12-14 16:58:36 +0300

    1

--- main copy 2.py
+++ main copy 2.py
@@ -1,4 +1,4 @@
-from os import remove
+from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
@@ -11,43 +11,67 @@ import math
 
 
 class Condition(Widget):
-    
+      
     def __init__(self, **kwargs):
         super(Condition, self).__init__(**kwargs)
-
+        
         self.count = 0
         self.radius = 50
         self.elps = []
         self.check_elps = None
         self.line = None
-    '''
+        self.active_elp = None
+
+        Window.bind(mouse_pos=self.on_motion)
+    
+    def on_motion(self, window, pos):
+        for elp in self.elps:
+            x0, y0 ,x1, y1 = elp.pos[0], elp.pos[1], pos[0], pos[1]
+            if math.sqrt((x1-x0-self.radius)**2+(y1-y0-self.radius)**2) <= self.radius:
+                if not self.line:
+                    with self.canvas:
+                        Color(1,0,0)
+                        self.line = Line(circle = (elp.pos[0]+50, elp.pos[1]+50, self.radius-2), width = 2)
+                        
+                self.active_elp = elp
+        
+        if self.active_elp:
+            if math.sqrt((x1- self.active_elp.pos[0]-self.radius)**2+(y1-self.active_elp.pos[1]-self.radius)**2) > self.radius:
+                self.active_elp = None
+                self.canvas.remove(self.line)
+                self.line = None
+
+
+    def on_touch_down(self, touch):
+        '''
+        if self.active_elp:
+            if self.line:
+                self.canvas.remove(self.line)
+            with self.canvas:
+                Color(1,1,0)
+                self.line = Line(circle = (self.active_elp.pos[0]+50, self.active_elp.pos[1]+50, self.radius-2), width = 2)
+                self.check_elps = self.active_elp
+        return super().on_touch_down(touch)
+        '''
 
     def on_touch_move(self, touch):
         #на рефакирпе сделатьпередачу через touch.ud["select_elp"] и self.collide_point
-        for elp in self.elps:
+        if self.active_elp:
+            elp = self.active_elp
             x0, y0 ,x1, y1 = elp.pos[0], elp.pos[1], touch.x, touch.y
             # разница между курсором и центром круга
             ox = x1-x0-self.radius
             oy = y1-y0-self.radius
-            # проверка на попадание мышкой в состояние
-            if math.sqrt(ox**2+oy**2) <= self.radius:
-                # новое положение состояния
-                elp.pos = [touch.x-self.radius, touch.y-self.radius]
-                # перемещение выделителя
-                points = self.line.points[:]
-                for i in range(len(points)):
-                    if not i % 2:
-                        points[i] += ox
-                        points[i + 1] += oy
+            # новое положение состояния
+            elp.pos = [touch.x-self.radius, touch.y-self.radius]
+            # перемещение выделителя
+            points = self.line.points[:]
+            for i in range(len(points)):
+                if not i % 2:
+                    points[i] += ox
+                    points[i + 1] += oy
                 self.line.points = points
         return super().on_touch_down(touch)
-    '''
-
-
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            touch.grab(self)
-            print('yes')
 
 
 class LaplaceApp(App):
