1df5e666de5d85d8db825dd6ff6821c2fd6e92a2
OlegShir <63945818+OlegShir@users.noreply.github.com>
2021-12-25 16:46:15 +0300

    1

--- main.py
+++ main.py
@@ -1,105 +1,66 @@
-from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.graphics import Ellipse
-from kivy.graphics import Color
-from kivy.graphics import Line
-import math
-from kivy.uix.screenmanager import ScreenManager, Screen 
+from functools import partial
 
 
-class MainScreen(Screen):
-    def on_mouse_pos(self, pos):
-        pass
-
-    Window.bind(mouse_pos = on_mouse_pos)
-
-class Condition(Widget):
-    
+class DemoBox(BoxLayout):
+    """
+    This class demonstrates various techniques that can be used for binding to
+    events. Although parts could me made more optimal, advanced Python concepts
+    are avoided for the sake of readability and clarity.
+    """
     def __init__(self, **kwargs):
-        super(Condition, self).__init__(**kwargs)
+        super(DemoBox, self).__init__(**kwargs)
+        self.orientation = "vertical"
 
-        self.count = 0
-        self.radius = 50
-        self.elps = []
-        self.check_elps = None
-        self.line = None
-   
+        # We start with binding to a normal event. The only argument
+        # passed to the callback is the object which we have bound to.
+        btn = Button(text="Normal binding to event")
+        btn.bind(on_press=self.on_event)
 
-    def on_touch_move(self, touch):
-        #на рефакирпе сделатьпередачу через touch.ud["select_elp"] и self.collide_point
-        for elp in self.elps:
-            x0, y0 ,x1, y1 = elp.pos[0], elp.pos[1], touch.x, touch.y
-            # разница между курсором и центром круга
-            ox = x1-x0-self.radius
-            oy = y1-y0-self.radius
-            # проверка на попадание мышкой в состояние
-            if math.sqrt(ox**2+oy**2) <= self.radius:
-                # новое положение состояния
-                elp.pos = [touch.x-self.radius, touch.y-self.radius]
-                # перемещение выделителя
-                points = self.line.points[:]
-                for i in range(len(points)):
-                    if not i % 2:
-                        points[i] += ox
-                        points[i + 1] += oy
-                self.line.points = points
-        return super().on_touch_down(touch)
+        # Next, we bind to a standard property change event. This typically
+        # passes 2 arguments: the object and the value
+        btn2 = Button(text="Normal binding to a property change")
+        btn2.bind(state=self.on_property)
+
+        # Here we use anonymous functions (a.k.a lambdas) to perform binding.
+        # Their advantage is that you can avoid declaring new functions i.e.
+        # they offer a concise way to "redirect" callbacks.
+        btn3 = Button(text="Using anonymous functions.")
+        btn3.bind(on_press=lambda x: self.on_event(None))
+
+        # You can also declare a function that accepts a variable number of
+        # positional and keyword arguments and use introspection to determine
+        # what is being passed in. This is very handy for debugging as well
+        # as function re-use. Here, we use standard event binding to a function
+        # that accepts optional positional and keyword arguments.
+        btn4 = Button(text="Use a flexible function")
+        btn4.bind(on_press=self.on_anything)
+
+        # Lastly, we show how to use partial functions. They are sometimes
+        # difficult to grasp, but provide a very flexible and powerful way to
+        # reuse functions.
+        btn5 = Button(text="Using partial functions. For hardcores.")
+        btn5.bind(on_press=partial(self.on_anything, "1", "2", monthy="python"))
+
+        for but in [btn, btn2, btn3, btn4, btn5]:
+            self.add_widget(but)
+
+    def on_event(self, obj):
+        print("Typical event from", obj)
+
+    def on_property(self, obj, value):
+        print("Typical property change from", obj, "to", value)
+
+    def on_anything(self, *args, **kwargs):
+        print('The flexible function has *args of', str(args),
+            "and **kwargs of", str(kwargs))
 
 
-    def on_touch_down(self, touch):
-        
-        
-        for elp in self.elps:
-            x0, y0 ,x1, y1 = elp.pos[0], elp.pos[1], touch.x, touch.y
-            if math.sqrt((x1-x0-self.radius)**2+(y1-y0-self.radius)**2) <= self.radius:
-                touch.ud["select_elp"] = elp
-                if self.check_elps != elp:
-                    if self.line:
-                        self.canvas.remove(self.line)
-                    with self.canvas:
-                        Color(1,1,0)
-                        self.line = Line(circle = (elp.pos[0]+50, elp.pos[1]+50, self.radius-2), width = 2)
-                    self.check_elps = elp
-        return super().on_touch_down(touch)
-
-
-class LaplaceApp(App):
+class DemoApp(App):
     def build(self):
-        parent = Widget()
-        self.painter = Condition()
-        parent.add_widget(self.painter)
+        return DemoBox()
 
-        parent.add_widget(Button(text = 'Добавить\n состояние', halign='center', on_press=self.add_condition, size =(100,50)))
-        parent.add_widget(Button(text = 'Удалить\n состояние', halign='center', on_press=self.del_condition, pos  = (105,0), size =(100,50)))
-        parent.add_widget(Button(text = 'Добавить\n переход', halign='center', on_press=self.add_connection, pos  = (210,0), size =(100,50)))
-        
-        return parent
-
-    def add_condition(self, instance):
-        with self.painter.canvas:
-            Color(1,1,1)
-            self.painter.elps.append(Ellipse(pos=(200,200), size=(2*self.painter.radius,2*self.painter.radius)))
-            self.painter.count += 1
-
-    def del_condition(self, instance):
-        if self.painter.elps and self.painter.check_elps:
-            #получаем инднкс выбранного состояния
-            index_elp = self.painter.elps.index(self.painter.check_elps)
-            #удаляем из спсиска
-            self.painter.elps.pop(index_elp)
-            #удаляем из канваса состаяние и выделитель
-            self.painter.canvas.remove(self.painter.check_elps)
-            self.painter.canvas.remove(self.painter.line)
-            self.painter.count -= 1
-
-    def add_connection(self, instance):
-        if self.painter.check_elps:
-            pass
-
- 
-if __name__ == '__main__':
-    LaplaceApp().run()
+if __name__ == "__main__":
+    DemoApp().run()
