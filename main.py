a0a52865f01cee620847e6ac03538d699ba900f6
OlegShir <63945818+OlegShir@users.noreply.github.com>
2022-06-20 00:34:29 +0300

    bezie

--- main.py
+++ main.py
@@ -1,66 +1,266 @@
-from kivy.uix.boxlayout import BoxLayout
+from re import A
+from kivy.core.window import Window
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from functools import partial
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Ellipse, Color, Bezier, Line, Triangle
+import copy
 
 
-class DemoBox(BoxLayout):
-    """
-    This class demonstrates various techniques that can be used for binding to
-    events. Although parts could me made more optimal, advanced Python concepts
-    are avoided for the sake of readability and clarity.
-    """
+import math
+from math_func import Geometric, Matrix
+geo = Geometric()
+
+
+class Condition(Widget):
+      
     def __init__(self, **kwargs):
-        super(DemoBox, self).__init__(**kwargs)
-        self.orientation = "vertical"
+        super(Condition, self).__init__(**kwargs)
+        
+        self.count = 0
+        self.radius = 50
+        self.labels =[]
 
-        # We start with binding to a normal event. The only argument
-        # passed to the callback is the object which we have bound to.
-        btn = Button(text="Normal binding to event")
-        btn.bind(on_press=self.on_event)
+        self.elps = []     
+        self.active_elp = None
 
-        # Next, we bind to a standard property change event. This typically
-        # passes 2 arguments: the object and the value
-        btn2 = Button(text="Normal binding to a property change")
-        btn2.bind(state=self.on_property)
+        self.connector = None
+        self.active_conn = None
 
-        # Here we use anonymous functions (a.k.a lambdas) to perform binding.
-        # Their advantage is that you can avoid declaring new functions i.e.
-        # they offer a concise way to "redirect" callbacks.
-        btn3 = Button(text="Using anonymous functions.")
-        btn3.bind(on_press=lambda x: self.on_event(None))
+        self.start_draw_bezie = False
+        self.bezierline_array =[]
+        self.bezierline_conn_array = []
+        self.bezierline_conn_move = False
+        self.active_bezierline_conn = None
 
-        # You can also declare a function that accepts a variable number of
-        # positional and keyword arguments and use introspection to determine
-        # what is being passed in. This is very handy for debugging as well
-        # as function re-use. Here, we use standard event binding to a function
-        # that accepts optional positional and keyword arguments.
-        btn4 = Button(text="Use a flexible function")
-        btn4.bind(on_press=self.on_anything)
+        self.triangle_array = []
+        self.check_elps = None
+        self.check_elps_line = None
 
-        # Lastly, we show how to use partial functions. They are sometimes
-        # difficult to grasp, but provide a very flexible and powerful way to
-        # reuse functions.
-        btn5 = Button(text="Using partial functions. For hardcores.")
-        btn5.bind(on_press=partial(self.on_anything, "1", "2", monthy="python"))
-
-        for but in [btn, btn2, btn3, btn4, btn5]:
-            self.add_widget(but)
-
-    def on_event(self, obj):
-        print("Typical event from", obj)
-
-    def on_property(self, obj, value):
-        print("Typical property change from", obj, "to", value)
-
-    def on_anything(self, *args, **kwargs):
-        print('The flexible function has *args of', str(args),
-            "and **kwargs of", str(kwargs))
+        # отслеживание курсора мышки
+        Window.bind(mouse_pos=self.on_motion)
 
 
-class DemoApp(App):
+    def on_motion(self, window, touch):
+        """Метод отслеживания перемещения мышки по рабочей области.
+        """
+        if self.active_elp:           
+            # если курсор выходит за радиус элепса + 10 рх
+            if not geo.cross_cursor(self.active_elp.pos, touch, self.radius, dopusk=20):
+                # удаление конекторов
+                for conn in self.connector: self.canvas.remove(conn)
+                # очистка переменных
+                self.connector = None
+                self.active_elp = None
+                self.active_conn = None
+            else:
+                if not self.active_conn:
+                    # если попадаем в конектор
+                    for i, conn in enumerate(self.connector):
+                        if geo.cross_cursor(conn.pos, touch, 5, dopusk=5):
+                            self.active_conn = conn
+                            Window.set_system_cursor("hand")
+                else:
+                    if not geo.cross_cursor(self.active_conn.pos, touch, 5, dopusk=5):
+                        self.active_conn = None
+                        Window.set_system_cursor("arrow")
+        else:
+            for elp in self.elps:
+                # ищем пересечение                
+                if geo.cross_cursor(elp.pos, touch, self.radius):
+                    # рисуем коннекторы
+                    if not self.connector:
+                        self.connector = []
+                        center_conn = geo.connector_pos(elp.pos)
+                        with self.canvas:
+                            Color(1,0,0)
+                            for conn in center_conn:
+                                self.connector.append(Ellipse(pos=(conn[0],conn[1]), size=(self.radius-40,self.radius-40)))
+                    # запоминаем выбранное состояние            
+                    self.active_elp = elp
+            if not self.active_elp and self.bezierline_conn_array != []:
+                for bizier_conn in self.bezierline_conn_array:
+                    if geo.cross_cursor(bizier_conn.pos, touch, 10, dopusk=5):
+                        Window.set_system_cursor("hand")
+                        self.bezierline_conn_move = True
+                        self.active_bezierline_conn = bizier_conn
+                    else:
+                        Window.set_system_cursor("arrow")
+                        self.bezierline_conn_move = False
+                        self.active_bezierline_conn = None
+                        
+
+
+    def on_touch_down(self, touch):
+        #блокировка при соединении элипсов
+        if self.active_conn:
+            self.start_draw_bezie = True
+            # сохранение позиции коннектора от которова производится соединение
+            touch.ud['position_active_conn'] = self.active_conn.pos
+            # сохранение номера элипса от которова производится соединение
+            touch.ud['number_start_elp'] = self.elps.index(self.active_elp)
+            # отображение "временной" линии Безье и ее центра
+            with self.canvas:
+                Color(1,0,0)
+                self.bezierline = Bezier()
+                self.bezierline_conn = Ellipse(size=(5,5) )
+        # выделение состояния
+        if self.active_elp and self.active_elp != self.check_elps:
+            if self.check_elps: self.canvas.remove(self.check_elps_line)
+            with self.canvas:
+                Color(1,1,0)
+                self.check_elps_line = Line(circle=(self.active_elp.pos[0]+self.radius, self.active_elp.pos[1]+self.radius, self.radius),width=3)
+                self.check_elps = self.active_elp
+        elif not self.active_elp :
+            if self.check_elps: 
+                self.canvas.remove(self.check_elps_line)
+                self.check_elps = None
+            if self.active_bezierline_conn:
+                print('Conn active')
+        
+      
+        return super().on_touch_down(touch)
+    
+    def on_touch_up(self, touch):
+
+        # при условии рисования линии Безье
+        if self.start_draw_bezie:
+            self.start_draw_bezie = False
+
+            # если конец линии Безье не соединен с коннектором
+            if self.active_conn:
+                # сохранение номера элипса с которым производится соединение
+                touch.ud['number_finish_elp'] = self.elps.index(self.active_elp)
+                # запись координат начало и конца линии Безье
+                point_triangle = geo.get_triangle_bezie_line(self.bezierline.points[0], self.bezierline.points[1], self.active_conn.pos[0]+5,self.active_conn.pos[1]+5)
+                with self.canvas:
+                    Color(1,1,0)
+                    self.triangle_now = Triangle(points = point_triangle)
+                    self.bezierline_now = Bezier(points=[self.bezierline.points[0], self.bezierline.points[1], touch.ud['bezie_3_point'][0], touch.ud['bezie_3_point'][1],self.active_conn.pos[0]+5,self.active_conn.pos[1]+5])
+                    self.bezierline_conn_now = Ellipse(pos = touch.ud['bezie_3_point'], size=(5,5) )
+                self.bezierline_array.append(self.bezierline_now)
+                self.bezierline_conn_array.append(self.bezierline_conn)
+                self.triangle_array.append(self.triangle_now)
+                # список номеров элепсов, которые соединяет линия Безье
+                conection = [touch.ud['number_start_elp'],touch.ud['number_finish_elp'] ]
+            
+            # удаление "временной" линии Безье
+            self.canvas.remove(self.bezierline)
+            self.canvas.remove(self.bezierline_conn)
+                
+ 
+        return super().on_touch_up(touch)
+
+    def on_touch_move(self, touch):
+        #на рефакирпе сделатьпередачу через touch.ud["select_elp"] и self.collide_point
+
+        # в случае если рисуется линия Безье
+        if self.start_draw_bezie:
+            # получение координат началалинии Безье            
+            x, y = touch.ud['position_active_conn']
+            points_bezierline = [x+5 ,y+5, touch.x, touch.y]
+            # перерисовка линии Безье
+            len_bezierline, points_bezier_conn = geo.middle_point(points_bezierline)
+            touch.ud['bezie_3_point']= points_bezier_conn
+            #print(  points_bezierline, '--------', points_bezier_conn )
+            if len_bezierline > 20:
+                self.bezierline_conn.pos = points_bezier_conn
+            self.bezierline.points = points_bezierline
+        
+        elif self.bezierline_conn_move:
+            # позиция выбранной точки линии бизье в списке
+            index = self.bezierline_conn_array.index(self.active_bezierline_conn)
+            x0, y0 ,x1, y1 = *self.active_bezierline_conn.pos, touch.x, touch.y
+            # разница между курсором и точки линии безье 
+            ox = x1-x0-10
+            oy = y1-y0-10
+            #получение линии безье, которая будет менятся
+            bezierline = self.bezierline_array[index]
+            # новое положение середины линии безье
+            points = [bezierline.points[0],bezierline.points[1],bezierline.points[2]+ox,bezierline.points[3]+oy,bezierline.points[4],bezierline.points[5]]
+            self.bezierline_array[index].points = points
+            # новое положение точки линии безье
+            opp = [x1-10, y1-10]
+            self.bezierline_conn_array[index].pos = opp
+            print(self.active_bezierline_conn.pos, '///', touch.x, '///', touch.y, index)
+            #изменение стрелки
+            point_triangle = geo.get_triangle_bezie_line(points[2],points[3],points[4],points[5])
+            self.triangle_array[index].points = point_triangle
+                            
+        # перемещение элипса
+        else:
+            if self.active_elp:
+                # позиция выбранного элипса  в списке
+                index = self.elps.index(self.active_elp)
+                # получаем координаты для пересчета движения       
+                x0, y0 ,x1, y1 = *self.active_elp.pos, touch.x, touch.y
+                # разница между курсором и центром элипса
+                ox = x1-x0-self.radius
+                oy = y1-y0-self.radius
+                # новое положение состояния
+                self.active_elp.pos = [touch.x-self.radius, touch.y-self.radius]
+                # новое положение обводки
+                points = self.check_elps_line.points[:]
+                for i in range(len(points)):
+                    if not i % 2:
+                        points[i] += ox
+                        points[i + 1] += oy
+                self.check_elps_line.points = points
+                # перемещение конекторов            
+                for conn in self.connector:
+                    conn.pos = (conn.pos[0]+ox, conn.pos[1]+oy)
+                # перемещение подписи
+                self.labels[index].pos = (self.labels[index].pos[0]+ox, self.labels[index].pos[1]+oy)
+
+
+
+                #print(x0, y0 ,x1, y1)
+
+        return super().on_touch_down(touch)
+
+class LaplaceApp(App):
     def build(self):
-        return DemoBox()
+        parent = Widget()
+        self.painter = Condition()
+        parent.add_widget(self.painter)
 
-if __name__ == "__main__":
-    DemoApp().run()
+        parent.add_widget(Button(text = 'Добавить\n состояние', halign='center', on_press=self.add_condition, size =(100,50)))
+        parent.add_widget(Button(text = 'Удалить\n состояние', halign='center', on_press=self.del_condition, pos  = (105,0), size =(100,50)))
+
+        #self.matrix = Matrix()
+        
+        return parent
+
+    def add_condition(self, instance):
+        with self.painter.canvas:
+            Color(1,1,1)
+            self.painter.elps.append(Ellipse(pos=(200,200), size=(2*self.painter.radius,2*self.painter.radius),))
+            self.painter.labels.append(Label(text=str(self.painter.count), font_size = 60, halign='left', pos=(200,200), color=(0,0,0)))
+        #self.matrix.extension()
+        self.painter.count += 1
+
+    def del_condition(self, instance):
+        if self.painter.elps and self.painter.check_elps:
+            #получаем инднкс выбранного состояния
+            index_elp = self.painter.elps.index(self.painter.check_elps)
+            #удаляем из спсиска
+            self.painter.elps.pop(index_elp)
+            self.painter.labels.pop(index_elp)
+            #удаляем из канваса состаяние и выделитель
+            self.painter.canvas.remove(self.painter.check_elps)
+            self.painter.check_elps = None
+            self.painter.canvas.remove(self.painter.check_elps_line)
+            self.painter.count -= 1
+            self.change_lable_index()
+            #self.matrix.compression()
+    
+    def change_lable_index(self):
+        index = 0
+        for label in self.painter.labels:
+            label.text = str(index)
+            index += 1
+
+if __name__ == '__main__':
+    LaplaceApp().run()
--- math_func.py
+++ math_func.py
@@ -15,8 +15,7 @@ class Geometric:
         if math.sqrt((x1-x0-radius)**2+(y1-y0-radius)**2) <= radius + dopusk:
             return True
         else:
-            return False
-      
+            return False 
 
     def connector_pos(self, ellips_pos):
         '''
@@ -29,6 +28,86 @@ class Geometric:
         conn_4 = (pos_x+45, pos_y+95)
 
         return [conn_1, conn_2, conn_3, conn_4]
+
+    def middle_point(self, array_points):
+        '''Определение середины отрезка'''
+        x0, y0, x1, y1 = array_points
+        x_mid = (x1+x0)/2
+        y_mid = (y1+y0)/2
+
+        len_line = math.sqrt((x1-x0)**2+(y1-y0)**2)
+
+        return len_line, [x_mid, y_mid]
+    
+    def turn_point_to_angle(self, x, y, x_centr, y_center, angle_rad):
+        '''Поворот точки на требуемый угол относительно выбранного угла'''
+        #angle = math.pi/180*angle
+        new_x = (x - x_centr) * math.cos(angle_rad) - (y - y_center) * math.sin(angle_rad) + x_centr
+        new_y = (x - x_centr) * math.sin(angle_rad) + (y - y_center) * math.cos(angle_rad) + y_center
+
+        return new_x, new_y
+
+    def angle_coeff(self, x0, y0, x1, y1):
+        '''Определения углового коэффициента отрезка'''
+        c = 0
+        # если отрезок уходит во 2 или 3 степень
+        if x1<x0:
+            c = math.pi
+        if x0==x1:
+            return 0
+        return math.atan((y1-y0)/(x1-x0))+c
+
+    def get_triangle_bezie_line(self, x_start_bezie, y_start_bezie, x_finish_bezie, y_finish_bezie):
+        '''Построение стрелок для линий Безье'''
+        b = 10
+        c = -10
+        #базовый треугольник
+        '''        
+        x3,y3 0000
+              0   0000
+              0       00   
+              0         00  x1,y1  
+              0       00  
+              0   0000 
+        x2,y2 0000
+        '''
+
+        x1 = x_finish_bezie
+        y1 = y_finish_bezie
+
+        x2 = x_finish_bezie - b
+        y2 = y_finish_bezie + c
+
+        x3 = x_finish_bezie - b
+        y3 = y_finish_bezie - c
+        #-----------------------
+        #определение угла поворота
+        angle = self.angle_coeff(x_start_bezie, y_start_bezie, x_finish_bezie, y_finish_bezie)
+        #определение новых точек поворота
+        x2,y2 = self.turn_point_to_angle(x2,y2,x1,y1,angle)
+        x3,y3 = self.turn_point_to_angle(x3,y3,x1,y1,angle)
+        
+        return [x1, y1, x2, y2, x3, y3]
+
+    def three_point_bezie(self, x0, y0, x1, y1, window_size):
+        '''Формула для 3-х точечной кривой Безье'''
+
+        min_array = [x1-x0,y1-y0]
+        min_value = min(min_array)
+        max_value = math.sqrt(window_size[0]**2 + window_size[1]**2)
+
+        
+        coef_hight = min_value*0.4*math.pi/180
+
+        x=(x1-x0)*math.cos(coef_hight)-(y1-y0)*math.sin(coef_hight)+x0
+        y=(x1-x0)*math.sin(coef_hight)+(y1-y0)*math.cos(coef_hight)+y0
+
+        ''' 
+        x=(x1-x0)*math.cos(math.pi/3)-(y1-y0)*math.sin(math.pi/3)+x0
+        y=(x1-x0)*math.sin(math.pi/3)+(y1-y0)*math.cos(math.pi/3)+y0
+        '''
+
+        return x,y
 
 class Matrix:
 
@@ -64,12 +143,8 @@ class Matrix:
 
 
 if __name__ == "__main__":
-    g = Matrix()
-    g.extension()
-    g.extension()
-    g.extension()
+    
+    
+    g = Geometric()
 
-    g.compression(2)
-
-    g.matrix[1,1]='None'
-    print(g.matrix)
+    print(g.angle_coeff(0,0,0,1))
